0edf6d3cdee62b895f7b4bd26b71a94c5d745a7e
FahadIqbal1122 <fahadiqbal.fd14@gmail.com>
2024-06-11 20:57:35 +0300

    expense routes and get

--- profitPulse/models/index.js
+++ profitPulse/models/index.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose")
 const userSchema = require("./User")
+const ExpenseSchema = require("./expense")
 
 const User = mongoose.model("User", userSchema)
+const Expense = mongoose.model("expense", ExpenseSchema)
 
 module.exports = {
   User,
+  Expense,
 }
