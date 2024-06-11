6fae4377e6eb59185e02d0dfdf66066d5d79a2cd
Afrah Mohammed <af.moali99@gmail.com>
2024-06-11 23:12:01 +0300

    update work to income

--- profitPulse/controllers/Expenses.js
+++ profitPulse/controllers/Expenses.js
@@ -1,28 +1,45 @@
-const Expense = require("../models/expense")
+const Income = require("../models/income")
 module.exports = {
   index,
   show,
+  new: newIncome,
 }
 // function Index - to create index
 async function index(req, res) {
   try {
-    // find expenses by id
-    let expenses = await Expense.find({}).populate("id")
-    res.render("expenses/index", { title: "All Expenses, expenses " })
+    // find incomes  by id
+    let incomes = await Income.find({}).populate("id")
+    res.render("income/index", { title: "All Incomes, incomes " })
   } catch (error) {
-    console.log("error fetching expenses:,", error)
+    console.log("error fetching incomes:,", error)
   }
 }
 
 async function show(req, res) {
   try {
-    // showing expenses byId (req.params.Id)
-    const expense = await Expense.findById(req.params.id).populate("id")
-    if (!expense) {
-      return res.render(404).send("Expense not found")
+    // showing incomes byId (req.params.Id)
+    const income = await Income.findById(req.params.id).populate("id")
+    if (!income) {
+      return res.render(404).send("Income not found")
     }
-    res.render("expenses/show", { title: "All Expenses Details, expense" })
+    res.render("income/show", { title: "All Income Details, income" })
   } catch (error) {
-    console.error("Error fetching expenses details:,error")
+    console.error("Error fetching incomes details:,error")
   }
+  // add Income
+}
+function newIncome(req, res) {
+  res.render("income/new", { title: "Add Income", errorMsg: "" })
+}
+async function createIncome(res, res) {
+  try {
+    // Create a new instance of the Income model using data from req.body
+    const income = new income(req.body)
+    if (income.name) {
+      income.name = income.name.toUpperCase()
+    }
+    if (income.price) {
+      income.price = income.price.toString()
+    }
+  } catch (error) {}
 }
