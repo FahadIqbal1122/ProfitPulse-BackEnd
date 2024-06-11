3fa9bb91f6496b42d9b8cf4c96472e67299f3ad3
Jellooking <jassimkhalaf14@gmail.com>
2024-06-11 11:34:49 +0300

    Create and connect db and modules

---
+++ profitPulse/controllers/AuthController.js
@@ -0,0 +1,55 @@
+const { User } = require('../models')
+const middleware = require('../middleware')
+const Register = async (req, res) => {
+  try {
+    // Extracts the necessary fields from the request body
+    const { email, password, name } = req.body
+    // Hashes the provided password
+    let passwordDigest = await middleware.hashPassword(password)
+    // Checks if there has already been a user registered with that email
+    let existingUser = await User.findOne({ email })
+    if (existingUser) {
+      return res
+        .status(400)
+        .send('A user with that email has already been registered!')
+    } else {
+      // Creates a new user
+      const user = await User.create({ name, email, passwordDigest })
+      // Sends the user as a response
+      res.send(user)
+    }
+  } catch (error) {
+    throw error
+  }
+}
+const Login = async (req, res) => {
+  try {
+    // Extracts the necessary fields from the request body
+    const { email, password } = req.body
+    // Finds a user by a particular field (in this case, email)
+    const user = await User.findOne({ email })
+    // Checks if the password matches the stored digest
+    let matched = await middleware.comparePassword(
+      user.passwordDigest,
+      password
+    )
+    // If they match, constructs a payload object of values we want on the front end
+    if (matched) {
+      let payload = {
+        id: user._id,
+        email: user.email
+      }
+      // Creates our JWT and packages it with our payload to send as a response
+      let token = middleware.createToken(payload)
+      return res.send({ user: payload, token })
+    }
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+  } catch (error) {
+    console.log(error)
+    res.status(401).send({ status: 'Error', msg: 'An error has occurred!' })
+  }
+}
+module.exports = {
+  Login,
+  Register
+}
