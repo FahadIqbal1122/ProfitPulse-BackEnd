51e5137775a76d670bf9ce8e575ab05d34602a24
FahadIqbal1122 <fahadiqbal.fd14@gmail.com>
2024-06-11 11:33:43 +0300

    Fix errors

---
+++ profitPulse/models/expense.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema
+
+const expenseSchema = new Schema({
+  note: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  userId: { type: Schema.Types.ObjectId, ref: "User" },
+  income: { type: Schema.Types.Number, ref: "Income" },
+  budget: { type: Schema.Types.Number, ref: "Budget" },
+})
+
+module.exports = mongoose.model("Expense", expenseSchema)
