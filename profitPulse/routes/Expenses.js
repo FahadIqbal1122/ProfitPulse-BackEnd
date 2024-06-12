26ecb1f9c20e6181c816117db0e532db042c899e
FahadIqbal1122 <fahadiqbal.fd14@gmail.com>
2024-06-12 11:11:02 +0300

    Complete Expense crud

--- profitPulse/routes/Expenses.js
+++ profitPulse/routes/Expenses.js
@@ -1,7 +1,9 @@
 const router = require("express").Router()
 const controller = require("../controllers/Expenses")
 
-router.get("/", controller.GetExpense)
+router.get("/", controller.GetExpenses)
 router.post("/", controller.CreateExpense)
+router.put("/:expense_id", controller.UpdateExpense)
+router.delete("/expense_id", controller.DeleteExpense)
 
 module.exports = router
