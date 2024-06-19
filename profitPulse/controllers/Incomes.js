2b48e784a4c1fec42a9a57c2aab6ae96fb4191ce
Afrah Mohammed <af.moali99@gmail.com>
2024-06-19 15:36:24 +0300

    -

--- profitPulse/controllers/Incomes.js
+++ profitPulse/controllers/Incomes.js
@@ -1,4 +1,4 @@
-const { Income, User } = require("../models")
+const { Income } = require("../models")
 
 // GetIncome
 const GetIncome = async (req, res) => {
@@ -10,16 +10,13 @@ const GetIncome = async (req, res) => {
     console.error("Error fetching incomes:", error)
   }
 }
-//test
+
 // createIncome
 const createIncome = async (req, res) => {
   try {
-    const userId = req.body.userId
-    const income = new Income({ ...req.body, userId })
+    const userId = req.userId
+    const income = new Income({ ...req.body })
     const savedIncome = await income.save()
-    const user = await User.findById(userId)
-    user.totalIncome = user.totalIncome + income.amount
-    await user.save()
     res.send(savedIncome)
   } catch (error) {
     console.error("Error creating income:", error)
@@ -28,55 +25,27 @@ const createIncome = async (req, res) => {
 
 const deleteIncome = async (req, res) => {
   try {
-    const incomeId = req.params.income_id
-    const income = await Income.findById(incomeId).populate("userId")
-    const user = income.userId
-    user.totalIncome = user.totalIncome - income.amount
-    await user.save()
-    await Income.deleteOne({ _id: incomeId })
+    await Income.deleteOne({ _id: req.params.income_id })
     res.send({
       msg: "Income Deleted",
-      payload: incomeId,
+      payload: req.params.income_id,
       status: "Ok",
     })
   } catch (error) {
-    console.error("Error deleting income:", error)
+    throw error
   }
 }
 
 const updateIncome = async (req, res) => {
   try {
-    const incomeId = req.params.income_id
-    const incomeToUpdate = await Income.findById(incomeId).populate("userId")
-
-    if (!incomeToUpdate) {
-      console.log("Income not found")
-    }
-    const user = await User.findOne({ _id: incomeToUpdate.userId })
-    console.log(`user ${user}`)
-    let updateAmount = req.body.amount
-
-    if (updateAmount) {
-      console.log(`updating user data`)
-      // incomeToUpdate.amount = updateAmount
-      const oldIncomeValue = incomeToUpdate.amount
-      console.log(`oldIncomeValue ${oldIncomeValue}`)
-      console.log(`updateAmount ${updateAmount}`)
-      console.log(
-        `user.totalIncome - oldIncomeValue + updateAmount ${
-          user.totalIncome - oldIncomeValue + updateAmount
-        }`
-      )
-      incomeToUpdate.amount = updateAmount
-      user.totalIncome = user.totalIncome - oldIncomeValue + updateAmount
-      console.log(`after user update ${user}`)
-    }
-
-    await user.save()
-    await incomeToUpdate.save()
-    res.send(incomeToUpdate)
+    const income = await Income.findByIdAndUpdate(
+      req.params.income_id,
+      req.body,
+      { new: true }
+    )
+    res.send(income)
   } catch (error) {
-    console.error("Error updating income:", error)
+    throw error
   }
 }
 
